0ab4ccf9e2eea6485960d1624eb65a1b7c69beb8
Kim van Wyk <kimv@sahomeloans.com>
2019-09-29 16:52:37 +0200

    Add basic auth

--- app/webserver.py
+++ app/webserver.py
@@ -1,5 +1,18 @@
+import json
+
 from flask import Flask, request
+from flask_basicauth import BasicAuth
+
 app = Flask(__name__)
+
+with open('credentials.json', 'r') as fh:
+    credentials = json.load(fh)
+
+app.config['BASIC_AUTH_USERNAME'] = credentials['username']
+app.config['BASIC_AUTH_PASSWORD'] = credentials['password']
+app.config['BASIC_AUTH_FORCE'] = True
+
+basic_auth = BasicAuth(app)
 
 @app.route('/reg_form_data', methods=['POST'])
 def reg_form_data():

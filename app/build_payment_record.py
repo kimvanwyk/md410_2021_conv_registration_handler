5029ba061a1e7d5386346b93014c6785d3980ffa
Kim van Wyk <kimv@sahomeloans.com>
2020-12-05 18:07:07 +0200

    Payment generation correc

--- app/build_payment_record.py
+++ app/build_payment_record.py
@@ -1,27 +1,28 @@
-""" Build an MDC2020 payment record from supplied reg num
+""" Build an MDC2021 payment record from supplied reg num
 
 """
 __author__ = "K van Wyk"
 __version__ = "0.0.1"
+from datetime import date
 import os, os.path
+
+
+from md410_2021_conv_common import db
+
+import create_payment_record_markdown
+from upload_data import parse_data_file
 
 import docker
 import pyperclip
 import s3
 
-MARKDOWN_CONTAINER = (
-    "markdown",
-    "registry.gitlab.com/md410_2020_conv/md410_2020_conv_payment_record_markdown_creator:latest",
-)
 PDF_CONTAINER = (
     "pdf",
-    "registry.gitlab.com/md410_2020_conv/md410_2020_conv_reg_form_pdf_creator:latest",
+    "registry.gitlab.com/md410_2021_conv/md410_2021_conv_pdf_creator:latest",
 )
-NETWORK = "container:md410_2020_conv_reg_form_server_config_postgres_1"
-QUEUE_NAME = "reg_form"
 
 
-def build_doc(reg_num, pull=False):
+def build_doc(registree_set, pull=False):
     client = docker.from_env()
 
     volumes = {os.getcwd(): {"bind": "/io", "mode": "rw"}}
@@ -30,40 +31,29 @@ def build_doc(reg_num, pull=False):
             print(f"Pulling {c[1]}")
             client.images.pull(c[1])
 
-    res = client.containers.run(
-        MARKDOWN_CONTAINER[1],
-        name=MARKDOWN_CONTAINER[0],
-        command=f"{reg_num}",
-        network=NETWORK,
-        volumes=volumes,
-        environment={'PGPASSWORD': os.getenv('PGPASSWORD')},
-        auto_remove=True,
-        stdout=True,
-        stderr=True,
-        tty=False,
-    ).decode("utf-8")
-    in_file = res.strip().split("/")[-1]
-    print(res, in_file)
-
+    fn = create_payment_record_markdown.main(
+        registree_set=registree_set, out_dir="."
+    ).split("/")[-1]
     res = client.containers.run(
         PDF_CONTAINER[1],
         name=PDF_CONTAINER[0],
-        command=f"/io/{in_file}",
-        network=NETWORK,
+        command=f"/io/{fn}",
         volumes=volumes,
         auto_remove=True,
         stdout=True,
         stderr=True,
         tty=False,
     ).decode("utf-8")
-    return f"{os.path.splitext(in_file)[0]}.pdf"
+    return f"{os.path.splitext(fn)[0]}.pdf"
 
 
-def process_reg_num(reg_num, pull=False):
+def process_reg_data(reg_num, pull=False):
     s = s3.S3(reg_num)
-    fn = build_doc(reg_num, pull)
+    dbh = db.DB()
+    registree_set = dbh.get_registrees(reg_num)
+    fn = build_doc(registree_set, pull)
     s.upload_pdf_file(fn)
-    print(f"processed reg num {reg_num}. File: {fn}")
+    print(f"Processed payment details for reg num {registree_set.reg_num}. File: {fn}")
     pyperclip.copy(f"evince {fn} &")
 
 
@@ -71,7 +61,13 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description=__doc__.split("\n")[0])
-    parser.add_argument("reg_num", type=int, help="Registration number")
-    parser.add_argument("--pull", action="store_true", help="Whether to also pull fresh containers")
+    parser.add_argument(
+        "reg_num",
+        type=int,
+        help="The registration number to download data from or reprocess",
+    )
+    parser.add_argument(
+        "--pull", action="store_true", help="Whether to also pull fresh containers"
+    )
     args = parser.parse_args()
-    process_reg_num(args.reg_num, args.pull)
+    process_reg_data(args.reg_num, pull=args.pull)
